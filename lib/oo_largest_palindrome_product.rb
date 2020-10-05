d378b4ad2571470251725bb14048d1c0eff86808
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-05 02:55:23 +0000

    Automatically backed up by Learn

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,14 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  attr_reader :
+  
+  
+  def initialize(input)
+    @product = product
+  end 
+  
+  def is_parlindrome?
+    if input.to_s.reverse == input.to_s
+      true
+  end
+end 
