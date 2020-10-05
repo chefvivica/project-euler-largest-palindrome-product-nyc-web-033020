8a829af539c216b06f5c38e2e229e514dbdd9310
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-05 03:00:13 +0000

    Automatically backed up by Learn

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,14 +1,31 @@
 # Implement your object-oriented solution here!
 class LargestPalindromeProduct
-  attr_reader :
+  attr_reader :answer, :palindrome_1, :palindrome_2
   
   
-  def initialize(input)
-    @product = product
+  def initialize
+    @answer = palindrome_product
+    @palindrome_1 = 0 
+    @palindrome_2 = 0
   end 
   
-  def is_parlindrome?
-    if input.to_s.reverse == input.to_s
-      true
-  end
+  def is_parlindrome?(input)
+    input.to_s == input.to_s.reverse
+  end 
+  
+  def palindrome_product
+    max = 0 
+    
+    (100..999).each do |first|
+      (100 .. 999).each do |second|
+        product = first * second
+        if is_palindrome?(product) && product > max
+          self.palindrome_1, self.palindrome_2 = first, second
+          max = product
+        end 
+      end 
+    end 
+    max
+  end 
 end 
+
